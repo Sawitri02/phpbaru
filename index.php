949c616b8982119226e6e87363f41422f09b7f5b
Sawitri02 <112366359+Sawitri02@users.noreply.github.com>
2022-11-26 06:48:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sejuta MAKANAN</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="wrapper">
+                <div class="logo"><a href=''>FOOD</a></div>
+                <div class="menu">
+                    <ul>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#menu">Menu</a></li>
+                        <li><a href="#service">Service</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <!-- <li><a href="" class="tbl-biru">Sign Up</a></li> -->
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header> 
+
+    <main>
+        <div class="wrapper">
+            <!-- untuk home -->
+            <section id="home">
+                <img src="image/Tumpeng.jpg" width="400" height="300"/>
+                <div class="kolom">
+                    <p class="deskripsi">Cintai makanan khas Indonesia</p>
+                    <h2>Tetap Sehat, Tetap Semangat</h2>
+                    <p>Indonesian food has a different taste because many kinds of special spices are mixed, don't believe it? Please try!</p>
+                    <p><a href="pessan.html" class="tbl-pink">Pesan disini</a></p>
+                </div>
+            </section>
+
+            <!-- untuk courses -->
+            <section id="about">
+                <div class="kolom">
+                    <p class="deskripsi">About Us</p>
+                    <!-- <h2>Online Courses</h2> -->
+                    <p> Ada banyak variasi makanan khas di Indonesia, hampir di setiap daerah di Indonesia memiliki makanan khas sendiri-sendiri. Makanan Indonesia sudah tidak dapat diragukan lagi tentang rasanya. Ketika kalian mencoba, pasti akan ketagihan.</p>
+                    <p><a href="" class="tbl-biru">Pelajari Lebih Lanjut</a></p>
+                </div>
+                <img src="image/Nasi Goreng.jpg" width="350" height="350"/>
+            </section>
+
+            <!-- untuk tutors -->
+            <section id="menu">
+                <div class="tengah">
+                    <div class="kolom">
+                        <p class="deskripsi">FOOD MENU</p>
+                        <h2>Great Price</h2>
+                        <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, optio!</p> -->
+                    </div>
+
+                    <div class="tutor-list">
+                        <div class="kartu-tutor">
+                            <img src="image/Rendang.jpg"/>
+                            <p>RENDANG</p>
+                            <p>25K</p>
+                        </div>
+                        <div class="kartu-tutor">
+                            <img src="image/Rawon.jpg"/>
+                            <p>RAWON</p>
+                            <p>25K</p>
+                        </div>
+                        <div class="kartu-tutor">
+                            <img src="image/Sate Madura.jpg"/>
+                            <p>SATE</p>
+                            <p>20K</p>
+                        </div>
+                        <div class="kartu-tutor">
+                            <img src="image/Martabak telur.jpg"/>
+                            <p>MARTABAK TELUR</p>
+                            <p>15K</p>
+                        </div>
+                    </div>
+                    <div class="tutor-list">
+                        <div class="kartu-tutor">
+                            <img src="image/Kerak telor.jpg"/>
+                            <p>KERAK TELUR</p>
+                            <p>15K</p>
+                        </div>
+                        <div class="kartu-tutor">
+                            <img src="image/Pempek.jpg"/>
+                            <p>PEMPEK</p>
+                            <p>20K</p>
+                        </div>
+                        <div class="kartu-tutor">
+                            <img src="image/Putu.jpg"/>
+                            <p>KUE PUTU</p>
+                            <P>15K</P>
+                        </div>
+                        <div class="kartu-tutor">
+                            <img src="image/Klepon.jpg"/>
+                            <p>KUE KLEPON</p>
+                            <P>15K</P>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- untuk partners -->
+            <section id="service">
+                <div class="tengah">
+                    <div class="kolom">
+                        <p class="deskripsi">Service</p>
+                        <h2>We provide best quality food</h2>
+                    </div>
+
+                    <div class="partner-list">
+                        <div class="kartu-partner">
+                            <img src=""/>
+                            <p>Order</p>
+                        </div>
+                        <div class="kartu-partner">
+                            <img src=""/>
+                            <p>Shipping</p>
+                        </div>
+                        <div class="kartu-partner">
+                            <img src=""/>
+                            <p>Delivered</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+
+    <footer>
+        <div id="contact">
+            <div class="wrapper">
+                <div class="footer">
+                    <div class="footer-section">
+                        <h3>Address</h3>
+                        <p>Ogan Komering Ilir, South Sumatra, Indonesia</p>
+                    </div>
+                    <div class="footer-section">
+                        <h3>Contact</h3>
+                        <a href="https://wa.me/6285731781787">
+                            <i class="fab fa-whatsapp"></i> 085731781787
+                        </a>
+                        <a href="mailto:witrisa02@gmail.com">
+                            <i class="fas fa-envelope"></i> witrisa02@gmail.com
+                        </a>
+                    </div>
+                    <!-- <div class="footer-section">
+                        <h3>Contact</h3>
+                        <a href="mailto:witrisa02@gmail.com">
+                            <i class="fas fa-envelope"></i> witrisa02@gmail.com
+                        </a>
+                    </div> -->
+                    <div class="footer-section">
+                        <h3>Social</h3>
+                        <a href="https://www.youtube.com/channel/UCqmTm6PoOf673ib7GqidKtA">Youtube: Sawitri Official</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
